98e4f291168535223501c121f9e953cd99f7cb20
Aly <aashour5@uwo.ca>
2023-12-02 14:26:11 -0500

    Side menu 😩

--- script.js
+++ script.js
@@ -48,3 +48,17 @@ function logout(event) {
     localStorage.removeItem('password');
     window.location.href = 'login.html';
 }
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("main").style.marginLeft = "350px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
